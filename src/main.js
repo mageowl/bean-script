392069b68cb06b4e258a8597edff661de34935fa
seattleowl <32573897+seattleowl@users.noreply.github.com>
2020-12-21 08:02:59 -0800

    Add web dectection.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 import { executer } from "./executer.js";
 import { lexer } from "./lexer.js";
 import { parser } from "./parser.js";
+import { isWeb } from "./web.js";
 
-window.addEventListener("load", () => {
-	let scripts = document.querySelectorAll('script[type="text/f-script"]');
+if (isWeb) {
+	console.log("FScript Web detected. Searching for script tags.");
+	window.addEventListener("load", () => {
+		let scripts = document.querySelectorAll('script[type="text/f-script"]');
 
-	// Compile scripts
-	scripts.forEach((scriptEl) => {
-		if (!scriptEl.src) {
-			let code = executer(parser(lexer(scriptEl.innerText)));
-		} else {
-			fetch(scriptEl.src)
-				.then((res) => res.text())
-				.then((text) => {
-					let code = executer(parser(lexer(text)));
-					console.log(JSON.stringify(code, null, 2));
-				});
-		}
+		// Compile scripts
+		scripts.forEach((scriptEl) => {
+			if (!scriptEl.src) {
+				let code = executer(parser(lexer(scriptEl.innerText)));
+			} else {
+				fetch(scriptEl.src)
+					.then((res) => res.text())
+					.then((text) => {
+						let code = executer(parser(lexer(text)));
+						console.log(JSON.stringify(code, null, 2));
+					});
+			}
+		});
 	});
-});
+} else {
+	console.warn(
+		"FScript could not detect web.\nNode.JS and other server-side javascript runtimes are currently not supported. (see https://github.com/seattleowl/f-script/issues/1.)"
+	);
+}
