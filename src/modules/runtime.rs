999d3e520042b907c779683c18f32a4b7dae3f51
Owen L. (WSL) <leggett.owen@gmail.com>
2024-02-04 18:18:00 -0800

    Add call and exists

--- src/modules/runtime.rs
+++ src/modules/runtime.rs
@@ -9,13 +9,16 @@ use crate::{
 use super::Module;
 
 pub fn construct(module: &mut Module) {
-	module
+	/* MEMORY */ module
 		.function("fn", fn_fn)
 		.function("let", fn_let)
-		.function("const", fn_const);
+		.function("const", fn_const)
+		.function("del", fn_del)
+		.function("call", fn_call)
+		.function("exists", fn_exists);
 }
 
-fn fn_fn(args: Vec<Data>, yield_fn: Option<Function>, _scope: Rc<RefCell<Scope>>) -> Data {
+fn fn_fn(args: Vec<Data>, yield_fn: Option<Function>, _s: Rc<RefCell<Scope>>) -> Data {
 	arg_check!(&args[0], Data::Memory { scope, name } => 
 		"Expected memory as name of function, but instead got {}.");
 	let yield_fn =
@@ -48,4 +51,26 @@ fn fn_const(args: Vec<Data>, yield_fn: Option<Function>, o_scope: Rc<RefCell<Sco
 	scope.borrow_mut().set_function(name, Function::Variable { value, constant: true, name: String::from(name) });
 
 	Data::None
+}
+
+fn fn_del(args: Vec<Data>, _y: Option<Function>, _s: Rc<RefCell<Scope>>) -> Data {
+	arg_check!(&args[0], Data::Memory { scope, name } => 
+		"Expected memory for fn del, but instead got {}.");
+	scope.borrow_mut().delete_function(name);
+
+	Data::None
+}
+
+fn fn_call(args: Vec<Data>, yield_fn: Option<Function>, o_scope: Rc<RefCell<Scope>>) -> Data {
+	arg_check!(&args[0], Data::Memory { scope, name } =>
+		"Expected memory for fn call, but instead got {}.");
+	let function = scope.borrow().get_function(name).unwrap_or_else(|| panic!("Unknown value or function for fn call '<{}>'.", &name));
+
+	function.call(args[1..].to_vec(), yield_fn, o_scope)
+}
+
+fn fn_exists(args: Vec<Data>, _y: Option<Function>, _s: Rc<RefCell<Scope>>) -> Data {
+	arg_check!(&args[0], Data::Memory { scope, name } =>
+		"Expected memory for fn exists, but instead got {}.");
+	Data::Boolean(scope.borrow().has_function(name))
 }
